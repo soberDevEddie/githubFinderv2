b99f00205f1e4e35154955255f58893f9bcc8205
EddieO23 <eocampo52@gmail.com>
2026-07-28 00:15:23 -0500

    added api token for follow and unfollow github user. next steps: start working on actual follow feature

--- src/api/github.js
+++ src/api/github.js
@@ -21,3 +21,24 @@ export const searchGithubUser = async (query) => {
   // console.log('Fetched user data:', data);
   return data.items;
 };
+
+// Check if following a user on Github
+export const checkIfFollowingUser = async (username) => {
+  const res = await fetch(
+    `${import.meta.env.VITE_GITHUB_API_URL}/user/following/${username}`,
+    {
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_GITHUB_API_TOKEN}`,
+        Accept: `application/vnd.github+json`,
+      },
+    },
+  );
+  if (res.status === 204) {
+    return true; // Following
+  } else if (res.status === 404) {
+    return false; // Not following
+  } else {
+    const errorData = await res.json().catch(() => null);
+    throw new Error('Failed to check following status' || errorData?.message);
+  }
+};
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,19 +1,45 @@
-import { FaGithubAlt } from 'react-icons/fa';
+import { FaUserMinus, FaUserPlus, FaGithubAlt } from 'react-icons/fa';
+import { useQuery } from '@tanstack/react-query';
+
+// My files
+import { checkIfFollowingUser } from '../api/github';
 
 const UserCard = ({ user }) => {
+  const { data: isFollowing, refetch } = useQuery({
+    queryKey: ['follow-status', user.login],
+    queryFn: () => checkIfFollowingUser(user.login),
+    enabled: !!user.login,
+  });
+
   return (
     <div className='user-card'>
       <img className='avatar' src={user.avatar_url} alt={user.name} />
       <h2 className='name'>{user.name || user.login}</h2>
       <p className='bio'>{user.bio || 'No bio available.'}</p>
-      <a
-        href={user.html_url}
-        className='profile-btn'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        <FaGithubAlt /> View Profile
-      </a>
+      <div className='user-card-buttons'>
+        <button
+          className={`follow-btn ${isFollowing ? 'following' : ''}`}
+          onClick={refetch}
+        >
+          {isFollowing ? (
+            <>
+              <FaUserMinus className='follow-icon' /> Following
+            </>
+          ) : (
+            <>
+              <FaUserPlus className='follow-icon' /> Follow User
+            </>
+          )}
+        </button>
+        <a
+          href={user.html_url}
+          className='profile-btn'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <FaGithubAlt /> View Profile
+        </a>
+      </div>
     </div>
   );
 };
